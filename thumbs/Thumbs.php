ddeea0c30b09ad5164add09927a39f809d6ec6be
auzadventure@gmail.com <auzadventure@gmail.com>
2023-02-02 14:06:47 +0800

    updated thumbs for webp and black background

--- thumbs/Thumbs.php
+++ thumbs/Thumbs.php
@@ -8,13 +8,15 @@ use yii\helpers\Url;
 
 //creates a thumb of image from jpg JPG, PNG
 
+
 class Thumbs extends BaseObject {
 
+	// The folder path inside /web/ that your images belong to
 	CONST ImagePath = 'images';
-	CONST ThumbPath = 'thumbs';
 
 	// @$path 
 	// @return in same directory
+	// If PNG->JPG->Web
 	public static function create($path) {
 		$fullPath = Yii::getAlias('@webroot/' . self::ImagePath .'/' . $path);
 		$ext = pathinfo($fullPath, PATHINFO_EXTENSION);
@@ -22,24 +24,80 @@ class Thumbs extends BaseObject {
 		$dir = pathinfo($fullPath, PATHINFO_DIRNAME);
 
 		//check if jpg or png
-		if($ext =='jpg' || $ext =='JPG') $img=imagecreatefromjpeg($fullPath);
-		elseif($ext == 'png') $img=imagecreatefrompng($fullPath);
+		if($ext =='jpg' || $ext =='JPG') {
+			$img=imagecreatefromjpeg($fullPath);
+			$w = imagesx($img);
+			$h = imagesy($img);			
+			$webp=imagecreatetruecolor($w,$h);
+			
+			imagecopy($webp,$img,0,0,0,0,$w,$h);
+			imagewebp($webp,"{$dir}/{$baseName}_thumb.webp", 80);
+			
+			imagedestroy($img);
+			imagedestroy($webp);
+			
+			return true; 		
+		}
+
+		elseif($ext == 'png') {
+			$imageJpg = self::createJpgFromPng($fullPath);
+			self::create($imageJpg);
+		}
 		else Yii::warning("Thumbs->create unable to handle ext {$ext}.");
 
-		$w = imagesx($img);
-		$h = imagesy($img);			
-		$webp=imagecreatetruecolor($w,$h);
-		
-		imagecopy($webp,$img,0,0,0,0,$w,$h);
-		imagewebp($webp,"{$dir}/{$baseName}_thumb.webp", 80);
-		
-		imagedestroy($img);
-		imagedestroy($webp);
-		return true; 
 	}
 
-	//if thumb exists show webp thumb
+	// Changers a PNG -> Jpg 
+	// Fix the black background with white
+	// create {filename.jpg} in same directory
+	// return $filename.jpg
+	public static function createJpgFromPng($fullPath) {
+		//we go png -> jpg to handle the black background
+		
+
+		$img=imagecreatefrompng($fullPath);
+		$w = imagesx($img);
+		$h = imagesy($img);	
+		$canvas=imagecreatetruecolor($w,$h);
+		imagealphablending($canvas, TRUE);
+
+		$white = imagecolorallocate($canvas,  255, 255, 255);
+		imagefilledrectangle($canvas, 0, 0, $w, $h,$white);
+	
+		imagecopy($canvas,$img,0,0,0,0,$w,$h);
+		
+		$baseName = pathinfo($fullPath, PATHINFO_FILENAME);
+		$dir = pathinfo($fullPath, PATHINFO_DIRNAME);
+
+		imagejpeg($canvas,"{$dir}/{$baseName}.jpg",100);
+		imagedestroy($canvas);
+		
+		return "{$baseName}.jpg";		
+	}
+
+	//resizes a webp image. keeps aspect ratio 
+	public static function resizeWebp($path,$maxWidth) {
+		$fullPath = Yii::getAlias('@webroot/' . self::ImagePath .'/' . $path);
+		list($w, $h) = getimagesize($fullPath);
+		if($w < $maxWidth) return;
+
+		$ratio = round($h/$w,2);
+		$w_ = $maxWidth;
+		$y_ = $w_ * $ratio; 
+		
+
+		$canvas = imagecreatetruecolor($w_, $y_);
+		$img = imagecreatefromwebp($fullPath);
+		imagecopyresampled($canvas, $img, 0, 0, 0, 0, $w_, $y_, $w, $h);
+		imagewebp($canvas,$fullPath, 80);
+		imagedestroy($canvas);
+		
+	}
+
+
+	//if web thumb exists show .webp thumb
 	//else show original 
+	//Use it as Html::img($Thumb->show($path)) 
 	public static function show($path) {
 
 		$fullPath = Yii::getAlias('@webroot/' . self::ImagePath .'/' . $path);
@@ -50,8 +108,8 @@ class Thumbs extends BaseObject {
 
 		if(self::thumbExist($path)) {
 			//get path before filename 
-			$dir = str_replace($fileName,'',$path);
-			$dir = str_replace('/.'.$ext,'',$dir);
+			
+			$dir = str_replace($fileName.".".$ext,'',$path);
 			$fileName = "{$baseName}_thumb.webp";
 
 			return Yii::getAlias("@web/".self::ImagePath."/{$dir}/{$fileName}"); 
--- thumbs/readme.md
+++ thumbs/readme.md
@@ -1,9 +1,18 @@
 
 ## Create
+If PNG, will convert jpg then webp.
+Turns transparent background white
+
 ``` ->create($path)```
 
-## Show 
+## ResizeWebp
+Keeps aspect ratio and resizes a webp image
 
+## Show 
+if web thumb exists show .webp thumb
+else show original 
+Use it as Html::img($Thumb->show($path)) 
+``` ->show($path) ```
 
 
 ## thumbExist
